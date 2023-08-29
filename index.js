a445916119b32543a1ebaec32973bfd4e1cc5e76
pro-mannan <promasudbd@gmail.com>
2023-08-29 22:44:39 +0600

    update

---
+++ index.js
@@ -0,0 +1,9 @@
+const dev = () => {
+    return`i'm a Full Stack Developer`;
+}
+
+const info = (name, age) => {
+    return`my name is ${name}and i'm ${age} Years Old`;
+}
+
+module.expores = {dev, info};
